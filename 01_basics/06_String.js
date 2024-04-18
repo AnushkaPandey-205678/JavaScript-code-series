9a90f557612233567278d703c2f7682c05914b11
ANUSHKA PANDEY <91444794+AnushkaPandey-205678@users.noreply.github.com>
2024-04-18 14:17:56 +0000

    Strings

--- 01_basics/06_String.js
+++ 01_basics/06_String.js
@@ -16,15 +16,15 @@ const gameName = new String('hitesh-hc-com')
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4)//only take positive value as index and only print one less than the given end index
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-8, 4)//take both positive and negative---negative index means string from end(reverse)
 console.log(anotherString);
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());//trim all the spaces from starting as well as ending.
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
