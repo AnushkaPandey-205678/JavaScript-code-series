e4d1b391dc2b8943f6647a38114dc3dbb01f5f8c
ANUSHKA PANDEY <91444794+AnushkaPandey-205678@users.noreply.github.com>
2024-04-18 12:00:52 +0000

    Datatype summary

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+//This code creates confusion it sometimes changes null to number and some times gives values as Nan
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//same with this also
+//== equality check:Only checks for equality
+// === (Strict check):checks for both equality as well as same datatype.
+
+console.log("2" === 2);
